84f5401fec05e795ceae7958e69296a6cb861ecb
abir Hossain <abirhossain141866@gmail.com>
2023-07-18 11:56:14 +0600

    remove login and profile from task.js

--- routes/api/task.js
+++ routes/api/task.js
@@ -137,75 +137,7 @@ router.delete("/:id",authenticationToken, async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password, type, refreshToken } = req.body;
-    if (!type) {
-      res.status(401).json({ massage: "  type not found" });
-    } else {
-      if (type == "email") {
-        const user = await User.findOne({ email: email });
-        if (!user) {
-          res.json({ massage: "user not found" });
-        } else {
-          const isValidPassword = await bcrypt.compare(password, user.password);
-          if (!isValidPassword) {
-            res.status(401).json({ massage: "wrong password" });
-          } else {
-            getUserTokens(user, res);
-          }
-        }
-      } else {
-        // ? Refresh tokens
-        if (!refreshToken) {
-          res.status(401).json({ massage: "  refresh is not found" });
-        } else {
-          jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
-            if (err) {
-              res.status(401).json({ massage: "  Unauthorize" });
-            } else {
-              const id = payload.id;
-              const user = await User.findById(id);
-              if (!user) {
-                res.status(401).json({ massage: " Unauthorize" });
-              } else {
-                getUserTokens(user, res);
-              }
-            }
-          });
-        }
-      }
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: "   not found" });
-  }
-});
 
-router.get("/profile", authenticationToken, async (req, res) => {
-  try {
-    const id = req.user.id;
-    const user = await User.findById(id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ massage: " user not found" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: " something is wrong" });
-  }
-});
 
 module.exports = router;
 
-function getUserTokens(user, res) {
-  const accessToken = jwt.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET, { expiresIn: "20m" });
-  const refreshToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "2d" });
-
-  const userObj = user.toJSON();
-  userObj["accessToken"] = accessToken;
-  userObj["refreshToken"] = refreshToken;
-
-  res.status(200).json(userObj);
-}
