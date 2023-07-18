c34a1846771320fbfb385c774c754e9679518b92
abir Hossain <abirhossain141866@gmail.com>
2023-07-18 10:59:32 +0600

    user can see one of his created task

--- routes/api/task.js
+++ routes/api/task.js
@@ -7,7 +7,7 @@ const authenticationToken = require("../../middleware/auth");
 const User = require("../../model/User");
 const Task = require("../../model/Task");
 
-// ?Register a new user
+// ?crate  a new task by user Id
 
 router.post(
   "/",
@@ -39,29 +39,42 @@ router.post(
 
 // ? get all task user by user
 router.get("/", authenticationToken, async (req, res) => {
-  try {  
+  try {
     const id = req.user.id;
     const task = await Task.find({ userId: id });
     res.json(task);
   } catch (error) {
-    console.error(error);   
-    res.status(500).json({ massage: "  users not found" }); 
+    console.error(error);
+    res.status(500).json({ massage: "  users not found" });
   }
 });
 
-
 // ? status change
 
-router.put("/status/:id",authenticationToken, async (req, res) => {   
+router.put(
+  "/status/:id",
+  [
+    authenticationToken,
+    [
+      body("status", "status is required").notEmpty(),
+      body("status", "status is invalid ").isIn(["to-do", "in-progress", "done"]),
+    ],
+  ],
+
+  async (req, res) => {
     try {
-      const id = req.params.id;  
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors });
+      }
+      const id = req.params.id;
       const userId = req.user.id;
-  
-     const status= req.body.status;
-  
-      const task = await Task.findByIdAndUpdate({_id: id, userId: userId}, {status:status}, { new: true });    
+
+      const status = req.body.status;
+
+      const task = await Task.findByIdAndUpdate({ _id: id, userId: userId }, { status: status }, { new: true });
       if (task) {
-        res.json(task);  
+        res.json(task);
       } else {
         res.status(404).json({ massage: " task not found" });
       }
@@ -69,20 +82,21 @@ router.put("/status/:id",authenticationToken, async (req, res) => {
       console.error(error);
       res.status(500).json({ massage: " something is wrong" });
     }
-  });
+  }
+);
 
 // ? update User Done
 
-router.put("/:id",authenticationToken, async (req, res) => {   
+router.put("/:id", authenticationToken, async (req, res) => {
   try {
-    const id = req.params.id;  
+    const id = req.params.id;
     const userId = req.user.id;
 
-   const body= req.body;
-      
-    const task = await Task.findOneAndUpdate({_id: id, userId: userId}, body, { new: true });     
+    const body = req.body;
+
+    const task = await Task.findOneAndUpdate({ _id: id, userId: userId }, body, { new: true });
     if (task) {
-      res.json(task);  
+      res.json(task);
     } else {
       res.status(404).json({ massage: " task not found" });
     }
@@ -92,6 +106,22 @@ router.put("/:id",authenticationToken, async (req, res) => {
   }
 });
 
+// ? user can see of his created task/get one user by id
+router.get("/:id",authenticationToken, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userId=req.user.id;
+    const task = await Task.findOne({_id:id,userId:userId});
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).json({ massage: " Task not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: " something is wrong" });
+  }
+});
 
 router.post("/login", async (req, res) => {
   try {
@@ -99,7 +129,7 @@ router.post("/login", async (req, res) => {
     if (!type) {
       res.status(401).json({ massage: "  type not found" });
     } else {
-      if (type == "email") { 
+      if (type == "email") {
         const user = await User.findOne({ email: email });
         if (!user) {
           res.json({ massage: "user not found" });
@@ -141,22 +171,6 @@ router.post("/login", async (req, res) => {
 router.get("/profile", authenticationToken, async (req, res) => {
   try {
     const id = req.user.id;
-    const user = await User.findById(id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ massage: " user not found" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: " something is wrong" });
-  }
-});
-
-// ? Read find one  ID from postman
-router.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
     const user = await User.findById(id);
     if (user) {
       res.json(user);
